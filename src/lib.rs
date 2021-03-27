b92cee506b438d3aa915d33cd97f27799bb98fce
Oli Dacombe <olidacombe@gmail.com>
2021-03-27 23:48:31 +0000

    first sketch

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,65 @@
+use std::collections::HashMap;
+use std::ops::SubAssign;
+
 #[derive(Debug, PartialEq)]
 pub struct Partition {
     pub left: Vec<i64>,
     pub right: Vec<i64>,
 }
 
-impl Partition {
-    pub fn from_subset(collection: Vec<i64>, subset: Vec<i64>) -> Self {
-        let mut subset_clone = subset.clone();
-        let mut right: Vec<i64> = vec![];
+struct OccurrenceCount {
+    counts: HashMap<i64, i64>,
+}
 
-        for v in collection.iter().rev() {
-            match subset_clone.last() {
-                None => {
-                    break;
-                }
-                Some(x) if x == v => {
-                    subset_clone.pop();
-                }
-                Some(_) => {
-                    right.push(v.clone());
-                }
-            };
+impl OccurrenceCount {
+    pub fn from_vec(set: &Vec<i64>) -> Self {
+        let mut counts = HashMap::new();
+
+        for v in set {
+            let count = counts.entry(*v).or_insert(0);
+            *count += 1;
         }
 
-        Self {
-            left: subset,
-            right: vec![],
+        return Self { counts };
+    }
+
+    pub fn to_vec(&self) -> Vec<i64> {
+        let mut vec = vec![];
+        for (k, v) in self.counts.iter() {
+            for _ in 0..*v {
+                vec.push(*k);
+            }
+        }
+        vec
+    }
+}
+
+impl SubAssign for OccurrenceCount {
+    fn sub_assign(&mut self, other: Self) {
+        for (k, v) in other.counts.iter() {
+            if let Some(current_count) = self.counts.get_mut(k) {
+                *current_count = (*current_count - *v).max(0);
+            }
         }
     }
 }
 
-struct SubsetSearchOpts {
-    collection: Vec<i64>,
+impl Partition {
+    pub fn from_subset(collection: &Vec<i64>, subset: &Vec<i64>) -> Self {
+        let mut collection_counts = OccurrenceCount::from_vec(collection);
+        let subset_counts = OccurrenceCount::from_vec(subset);
+
+        collection_counts -= subset_counts;
+
+        Self {
+            left: subset.to_vec(),
+            right: collection_counts.to_vec(),
+        }
+    }
+}
+
+struct SubsetSearchOpts<'a> {
+    collection: &'a Vec<i64>,
     required_sum: i64,
     complement_not_exceeding: i64,
 }
@@ -43,10 +71,12 @@ fn get_subset_with_sum(
         complement_not_exceeding,
     }: SubsetSearchOpts,
 ) -> Option<Vec<i64>> {
-    if collection.len() > 1 {}
+    if required_sum == 0 {
+        return Some(vec![]);
+    }
     match collection.first() {
         None => match required_sum {
-            0 => Some(collection),
+            0 => Some(collection.to_vec()),
             _ => None,
         },
         _ => {
@@ -57,7 +87,7 @@ fn get_subset_with_sum(
                 let subset = match v {
                     x if x > required_sum => None,
                     _ => get_subset_with_sum(SubsetSearchOpts {
-                        collection: collection.clone(),
+                        collection: &collection,
                         required_sum: required_sum - v,
                         complement_not_exceeding,
                     }),
@@ -80,7 +110,7 @@ fn get_subset_with_sum(
     }
 }
 
-pub fn get_equi_partition(collection: Vec<i64>) -> Option<Partition> {
+pub fn get_equi_partition(collection: &Vec<i64>) -> Option<Partition> {
     if collection.is_empty() {
         return None;
     }
@@ -88,16 +118,16 @@ pub fn get_equi_partition(collection: Vec<i64>) -> Option<Partition> {
     collection.sort();
     collection.reverse();
     let sum: i64 = collection.iter().sum();
-    if sum % 1 == 1 {
+    if sum % 2 == 1 {
         return None;
     }
     let half_sum = sum / 2;
     match get_subset_with_sum(SubsetSearchOpts {
-        collection,
+        collection: &collection,
         required_sum: half_sum,
         complement_not_exceeding: half_sum,
     }) {
-        Some(subset) => Some(Partition::from_subset(collection, subset)),
+        Some(subset) => Some(Partition::from_subset(&collection, &subset)),
         _ => None,
     }
 }
@@ -108,17 +138,27 @@ mod tests {
 
     #[test]
     fn it_returns_none_when_no_equi_partition_exists() {
-        assert!(get_equi_partition(vec![]).is_none());
-        assert!(get_equi_partition(vec![2]).is_none());
-        assert!(get_equi_partition(vec![1, 2]).is_none());
-        assert!(get_equi_partition(vec![1, 1, 2, 6]).is_none());
-        assert!(get_equi_partition(vec![1, 2, 3, 4, 5]).is_none());
+        let test_cases = vec![
+            vec![],
+            vec![2],
+            vec![1, 2],
+            vec![1, 1, 2, 6],
+            vec![1, 2, 3, 4, 5],
+        ];
+        for test_case in test_cases {
+            let partition = get_equi_partition(&test_case);
+            assert!(
+                partition.is_none(),
+                "Found {:#?} - expected None",
+                partition
+            );
+        }
     }
 
     #[test]
     fn it_returns_equi_partition_when_exists() {
         assert_eq!(
-            get_equi_partition(vec![1, 1]),
+            get_equi_partition(&vec![1, 1]),
             Some(Partition {
                 left: vec![1],
                 right: vec![1]
@@ -127,11 +167,23 @@ mod tests {
 
         let test_cases = vec![vec![1, 2, 3], vec![1, 2, 2, 3], vec![10, 1, 2, 3, 4]];
 
-        for test_case in test_cases {
-            let partition = get_equi_partition(test_case).unwrap();
+        for mut test_case in test_cases {
+            let partition = get_equi_partition(&test_case).unwrap();
             let sum_left: i64 = partition.left.iter().sum();
-            let sum_right: i64 = partition.left.iter().sum();
-            assert_eq!(sum_left, sum_right);
+            let sum_right: i64 = partition.right.iter().sum();
+            assert_eq!(
+                sum_left, sum_right,
+                "sum({:#?}) != sum({:#?})",
+                partition.left, partition.right
+            );
+            let mut union = [&partition.left[..], &partition.right[..]].concat();
+            union.sort();
+            test_case.sort();
+            assert_eq!(
+                union, test_case,
+                "{:#?} is an invalid partition of {:#?}",
+                partition, test_case
+            );
         }
     }
 }
